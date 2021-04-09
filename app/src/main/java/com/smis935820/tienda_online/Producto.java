f95257b63429734c136e009170721350b5766e65
SrWyatt <esau_batres2001@hotmail.com>
2021-04-08 22:05:09 -0500

    Listview y menú de opciones. (Josué Esaú Batres Guardado, SMIS058820)

--- app/src/main/java/com/smis935820/tienda_online/Producto.java
+++ app/src/main/java/com/smis935820/tienda_online/Producto.java
@@ -3,15 +3,68 @@ package com.smis935820.tienda_online;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Producto extends AppCompatActivity {
+
+    DatabaseHandler DB;
+    public Cursor c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
+
+        showData();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        AdapterView.AdapterContextMenuInfo info=
+                (AdapterView.AdapterContextMenuInfo)menuInfo;
+        c.moveToPosition(info.position);
+        menu.setHeaderTitle("Opciones");
+    }
+
+    private void showData() {
+        DB = new DatabaseHandler(Producto.this);
+        c = DB.getData();
+
+        //Evaluar si existe registros
+
+        if (c.moveToFirst()) {
+            ListView listData = (ListView) findViewById(R.id.listData);
+
+            //Arraylist
+            final ArrayList<String> allData = new ArrayList<String>();
+            final ArrayAdapter<String> aData = new ArrayAdapter<String>(Producto.this, android.R.layout.simple_expandable_list_item_1, allData);
+
+            //Adaptador a ListView
+            listData.setAdapter(aData);
+
+            //Mostrar registros
+            do {
+                allData.add(c.getString(1));
+            } while (c.moveToNext());
+            registerForContextMenu(listData);
+        } else {
+            Toast.makeText(this, "No hay registros para mostrar", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 
     ///Pasar al registro
